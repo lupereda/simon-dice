5d5db53bf917f9f7d09bde33afaea2716455dedc
LuCIA P <PC@DESKTOP-6OJLNOO>
2025-12-15 15:05:36 -0300

    funciones para reproducir la secuencia , aumentar el scorey reiniciar el juego

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ function flashButton(color) {
         button.classList.remove('sequence-active');
     }, 600);
 }
+
+//funcion para iluminar un boton al hacer click
+function flashButtonClick(color) {
+    const button= document.getElementById(color);
+    button.classList.add('click-active');
+    setTimeout(() => {
+        button.classList.remove('click-active');
+    }, 300);
+}
+
+//funcion para reproducir la secuencia del jeugo
+function playSequence() {
+    let i=0;
+    const interval = setInterval(() => {
+        flashButton(sequence[i]);
+        i++;
+        if (i >= sequence.length) {
+            clearInterval(interval);
+            setTimeout(() => {
+            isPlayerTurn = true; //indico que es el turno del jugador
+            }, 500);
+        };
+    }, 800);
+}
+
+//funcion para aumentar el nivel y agregar un nuevo color a la secuencia
+function nextLevel() {
+    level++;
+    isPlayerTurn = false; //indico que no es el turno del jugador
+    userSequence = [];
+    sequence.push( getRandomColor() );
+    setTimeout( () => {
+        playSequence();
+    }, 1000);
+}
+
+//funcion para reiniciar el juego
+function resetGame() {
+    alert(`Game Over! You reached level ${level}.`);
+    sequence = [];
+    userSequence = [];
+    level = 0;
+    isPlayerTurn = false;
+}
